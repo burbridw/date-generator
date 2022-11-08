d206be63f5150522e72d248ea09b3965ad3f80bc
burbridw <111381782+burbridw@users.noreply.github.com>
2022-11-08 16:35:00 +0900

    button styling

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const yearsArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "DEL"]
 function generate() {
     for ( let i = 0; i < 7; i++ ) {
         daySettings.innerHTML += `
-        <div class="day-display">
+        <div class="day-display button">
         <div class="day-text">${daysArr[i]}</div>
         <div class="day-img">IMG</div>
         </div>
@@ -18,25 +18,64 @@ function generate() {
     }
     for ( let i = 0; i < 12; i++ ) {
         monthSettings.innerHTML += `
-        <div class="month-display">
+        <div class="month-display button">
         <div class="month-img">${monthsArr[i]}</div>
         </div>
         `
     }
     for ( let i = 0; i < 31; i++ ) {
         dateSettings.innerHTML += `
-        <div class="date-display">
+        <div class="date-display button">
         <div class="date-text">${datesArr[i]}</div>
         </div>
         `
     }
     for ( let i = 0; i < 11; i++ ) {
         yearSettings.innerHTML += `
-        <div class="year-display">
+        <div class="year-display button">
         <div class="year-text">${yearsArr[i]}</div>
         </div>
         `
     }
+    allButtons = document.querySelectorAll(".button")
+    allButtons.forEach( (x) => {
+        x.addEventListener("click",function() {
+            if ( x.classList.contains("active") ) {
+                x.classList.remove("active")
+            } else {
+                if ( x.classList.contains("day-display") ) {
+                    let allDayButtons = document.querySelectorAll(".day-display")
+                    allDayButtons.forEach( (y) => {
+                        if ( y.classList.contains("active") ) {
+                            y.classList.remove("active")
+                        }
+                    })
+                } else if ( x.classList.contains("month-display") ) {
+                    let allMonthButtons = document.querySelectorAll(".month-display")
+                    allMonthButtons.forEach( (y) => {
+                        if ( y.classList.contains("active") ) {
+                            y.classList.remove("active")
+                        }
+                    })
+                } else if ( x.classList.contains("date-display") ) {
+                    let allDateButtons = document.querySelectorAll(".date-display")
+                    allDateButtons.forEach( (y) => {
+                        if ( y.classList.contains("active") ) {
+                            y.classList.remove("active")
+                        }
+                    })
+                } else if ( x.classList.contains("year-display") ) {
+                    let allYearButtons = document.querySelectorAll(".year-display")
+                    allYearButtons.forEach( (y) => {
+                        if ( y.classList.contains("active") ) {
+                            y.classList.remove("active")
+                        }
+                    })
+                }
+                x.classList.add("active")
+            }
+        })
+    })
 }
 
 generate()
